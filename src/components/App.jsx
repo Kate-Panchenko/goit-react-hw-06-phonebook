f82d7bbd42fabd9ff9b0520aa3c036db478eb74f
Kate Panchenko <k.panchenko12345@gmail.com>
2023-04-05 15:23:43 +0300

    template done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { Layout } from "./Layout";
+import { theme } from "./Layout";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        <div></div>
+      </Layout>
+    </ThemeProvider>
   );
 };
